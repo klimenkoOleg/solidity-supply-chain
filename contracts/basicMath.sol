7b609ff1c2ed35cf5297c1ff4cda7882e987abf2
Oleg_Klimenko <oklimenko@gmail.com>
2022-07-16 22:06:19 -0700

    Moved to Solidity 0.6.0

--- contracts/basicMath.sol
+++ contracts/basicMath.sol
@@ -1,4 +1,7 @@
-pragma solidity >=0.4.21 <0.6.0;
+// SPDX-License-Identifier: ECL-1.0
+// Copyright (c) LinkedIn learning
+// This code is my learning course from LinkedIn learning https://www.linkedin.com/learning/building-an-ethereum-blockchain-app-8-supply-chain-smart-contract-dapp
+pragma solidity >=0.4.21 <0.9.0;
 
 contract basicMath {
     uint256 constant private MAX_UINT256 = 2**256 - 1;
--- contracts/erc20Interface.sol
+++ contracts/erc20Interface.sol
@@ -1,18 +1,21 @@
+// SPDX-License-Identifier: ECL-1.0
+// Copyright (c) LinkedIn learning
+// This code is my learning course from LinkedIn learning https://www.linkedin.com/learning/building-an-ethereum-blockchain-app-8-supply-chain-smart-contract-dapp
 // ----------------------------------------------------------------------------
 // ERC Token Standard #20 Interface
 // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
 // ----------------------------------------------------------------------------
-pragma solidity >=0.4.21 <0.6.0;
+pragma solidity >=0.4.21 <0.9.0;
 
-contract ERC20Interface {
+abstract contract ERC20Interface {
     uint256 public totSupply;
     
-    function totalSupply() public view returns (uint);
-    function balanceOf(address tokenOwner) public view returns (uint balance);
-    function allowance(address tokenOwner, address spender) public view returns (uint remaining);
-    function transfer(address to, uint tokens) public returns (bool success);
-    function approve(address spender, uint tokens) public returns (bool success);
-    function transferFrom(address from, address to, uint tokens) public returns (bool success);
+    function totalSupply() external virtual view returns (uint);
+    function balanceOf(address tokenOwner) external virtual view returns (uint balance);
+    function allowance(address tokenOwner, address spender) external virtual view returns (uint remaining);
+    function transfer(address to, uint tokens) external virtual returns (bool success);
+    function approve(address spender, uint tokens) external virtual returns (bool success);
+    function transferFrom(address from, address to, uint tokens) external virtual returns (bool success);
 
     event Transfer(address indexed from, address indexed to, uint tokens);
     event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
